02d81c7eb51eb0795d231853de797fab387cf961
Vinícius Albert <vinifantal@hotmail.com>
2025-08-05 21:47:18 -0300

    add: implements Scores endpoints

---
+++ src/test/kotlin/endpoints/ScoreEndpointsTest.kt
@@ -0,0 +1,39 @@
+package endpoints
+
+import OsuKDK
+import credentials.Authorization
+import credentials.GrantType
+import credentials.ScopesEnum
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class ScoreEndpointsTest {
+
+    companion object {
+        private val api: OsuKDK by lazy {
+            val clientId = System.getenv("CLIENT_ID").toInt()
+            val clientSecret = System.getenv("CLIENT_SECRET")
+
+            runBlocking { Authorization(
+                clientId,
+                clientSecret,
+                grantType = GrantType.AUTHORIZATION_CODE,
+                scope = listOf(ScopesEnum.PUBLIC, ScopesEnum.IDENTIFY)).create() }
+        }
+    }
+
+    @Test
+    fun downloadScore() = runTest {
+        val bytes = api.downloadScore(4459998279, true)
+        assertNotNull(bytes)
+    }
+
+    @Test
+    fun getScore() = runTest {
+        val scores = api.getScore()
+        assertNotNull(scores)
+    }
+}
