d1544119f78660de1f066fa47de3d8c9f51c8c98
Vinícius Albert <vinifantal@hotmail.com>
2025-08-08 20:51:56 -0300

    add: dokka plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     kotlin("plugin.serialization") version "2.2.0"
     id("com.vanniktech.maven.publish") version "0.34.0"
     id("signing")
+    id("org.jetbrains.dokka") version "2.0.0"
     jacoco
 }
 
