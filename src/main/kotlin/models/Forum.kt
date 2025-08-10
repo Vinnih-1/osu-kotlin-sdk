304ac102bbaf7d4fa3953d6fe95f53607eb6d13b
Vinícius Albert <vinifantal@hotmail.com>
2025-08-09 23:40:29 -0300

    add: forum models

---
+++ src/main/kotlin/models/Forum.kt
@@ -0,0 +1,12 @@
+package models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Forum(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val subforums: List<Forum>? = null
+) {
+}
