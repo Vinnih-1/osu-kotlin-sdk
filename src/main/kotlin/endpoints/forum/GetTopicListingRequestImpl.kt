b4fcb1815eb93c30d118a3258fee2fd71602a98c
Vinícius Albert <vinifantal@hotmail.com>
2025-08-09 23:40:22 -0300

    add: forum endpoints

---
+++ src/main/kotlin/endpoints/forum/GetTopicListingRequestImpl.kt
@@ -0,0 +1,36 @@
+package endpoints.forum
+
+import OsuKDK.Companion.json
+import endpoints.EndpointRequest
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.serialization.Serializable
+import models.ForumTopic
+
+class GetTopicListingRequestImpl(
+    val forumId: String?,
+    val sort: String?,
+    val limit: Int?,
+    val cursorString: String?
+) : EndpointRequest<ForumTopicResponse> {
+
+    override fun endpoint(): String = "forums/topics"
+
+    override suspend fun request(client: HttpClient): ForumTopicResponse {
+        val response = client.get(this.url) {
+            parameter("forum_id", forumId)
+            parameter("sort", sort)
+            parameter("limit", limit)
+            parameter("cursor_string", cursorString)
+        }
+        return json.decodeFromString<ForumTopicResponse>(response.bodyAsText())
+    }
+
+}
+
+@Serializable
+data class ForumTopicResponse(
+    val topics: List<ForumTopic>,
+    val cursorString: String
+)
---
+++ src/main/kotlin/endpoints/forum/ReplyTopicRequestImpl.kt
@@ -0,0 +1,26 @@
+package endpoints.forum
+
+import OsuKDK.Companion.json
+import endpoints.EndpointRequest
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import models.ForumPost
+
+class ReplyTopicRequestImpl(
+    val topicId: Int,
+    val bodyString: String
+) : EndpointRequest<ForumPost> {
+    override fun endpoint(): String = "forums/topics/${topicId}/reply"
+
+    override suspend fun request(client: HttpClient): ForumPost {
+        val response = client.post(this.url) {
+            contentType(ContentType.Application.Json)
+            setBody(json.encodeToString(mapOf(
+                "body" to bodyString
+            )))
+        }
+        return json.decodeFromString<ForumPost>(response.bodyAsText())
+    }
+}
