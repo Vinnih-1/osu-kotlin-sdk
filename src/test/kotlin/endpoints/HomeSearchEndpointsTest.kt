7750a8c06d4278b079bb576ff70b98a1dac3e43e
Vinícius Albert <vinifantal@hotmail.com>
2025-08-10 20:27:56 -0300

    add: Home endpoint test

---
+++ src/test/kotlin/endpoints/HomeSearchEndpointsTest.kt
@@ -0,0 +1,16 @@
+package endpoints
+
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class HomeSearchEndpointsTest {
+
+    val api = OsuApiProvider.api
+
+    @Test
+    fun homeSearch() = runTest {
+        val result = api.search()
+        assertNotNull(result)
+    }
+}
