0a4c0cc89d1b6043b5a1ca4e44d50ffc420ebfbb
Vinícius Albert <vinifantal@hotmail.com>
2025-08-11 11:53:26 -0300

    add: ranking and wiki tests

---
+++ src/test/kotlin/endpoints/RankingEndpointsTest.kt
@@ -0,0 +1,28 @@
+package endpoints
+
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class RankingEndpointsTest {
+
+    val api = OsuApiProvider.api
+
+    @Test
+    fun getKudosuRanking() = runTest {
+        val ranking = api.getKudosuRanking()
+        assertNotNull(ranking)
+    }
+
+    @Test
+    fun getRankings() = runTest {
+        val rankings = api.getRanking(RankingType.PERFORMANCE, ModeEnum.OSU)
+        assertNotNull(rankings)
+    }
+
+    @Test
+    fun getSpotlights() = runTest {
+        val spotlights = api.getSpotlights()
+        assertNotNull(spotlights)
+    }
+}
---
+++ src/test/kotlin/endpoints/WikiPageEndpointTest.kt
@@ -0,0 +1,16 @@
+package endpoints
+
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class WikiPageEndpointTest {
+
+    val api = OsuApiProvider.api
+
+    @Test
+    fun getWikiPage() = runTest {
+        val page = api.getWikiPage("pt-br", "Client/File_formats")
+        assertNotNull(page)
+    }
+}
