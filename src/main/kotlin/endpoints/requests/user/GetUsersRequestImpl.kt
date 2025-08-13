9e7f74bef2cfc4a343d537ffa5ec62e2ca990b5a
Vinícius Albert <vinifantal@hotmail.com>
2025-08-12 21:29:17 -0300

    fix: return type

--- src/main/kotlin/endpoints/requests/user/GetUsersRequestImpl.kt
+++ src/main/kotlin/endpoints/requests/user/GetUsersRequestImpl.kt
@@ -2,21 +2,20 @@ package endpoints.requests.user
 
 import OsuKDK.Companion.json
 import endpoints.requests.EndpointRequest
+import endpoints.responses.users.UsersResponse
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import models.users.GetUsersResponse
-import models.users.User
 
-class GetUsersRequestImpl(val ids: List<String>, val includeVariantStatistics: Boolean?) : EndpointRequest<List<User>> {
+class GetUsersRequestImpl(val ids: List<String>, val includeVariantStatistics: Boolean?) : EndpointRequest<UsersResponse> {
 
     override fun endpoint(): String = "users/"
 
-    override suspend fun request(client: HttpClient): List<User> {
+    override suspend fun request(client: HttpClient): UsersResponse {
         val response = client.get(this.url) {
             ids.forEach { parameter("ids[]", it) }
             parameter("include_variant_statistics", includeVariantStatistics)
         }
-        return json.decodeFromString<GetUsersResponse>(response.bodyAsText()).users
+        return json.decodeFromString<UsersResponse>(response.bodyAsText())
     }
 }
