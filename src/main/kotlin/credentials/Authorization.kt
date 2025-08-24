f39ce71458ad50a8d471f0be426261ec25792456
Vinícius Albert <vinifantal@hotmail.com>
2025-08-24 18:31:16 -0300

    add: get a client passing accessToken

--- src/main/kotlin/credentials/Authorization.kt
+++ src/main/kotlin/credentials/Authorization.kt
@@ -5,85 +5,162 @@ import enums.GrantType
 import enums.ScopesEnum
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
+import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonNamingStrategy
+import kotlinx.serialization.json.JsonPrimitive
+import org.slf4j.LoggerFactory
 import java.awt.Desktop
 import java.net.InetAddress
 import java.net.ServerSocket
 import java.net.URI
 import java.util.*
 
-@Serializable
 @OptIn(ExperimentalSerializationApi::class)
-data class Authorization(
-    val clientId: Int,
-    val clientSecret: String,
-    var code: String = "",
-    val grantType: GrantType = GrantType.CLIENT_CREDENTIALS,
+class Authorization(
+    val clientId: Int? = null,
+    val clientSecret: String? = null,
+    val redirectUri: String? = null,
     val scope: List<ScopesEnum> = listOf(ScopesEnum.PUBLIC),
-    val redirectUri: String = "http://localhost:3914"
+    val grantType: GrantType = GrantType.CLIENT_CREDENTIALS,
+    var accessToken: String? = null,
+    var refreshToken: String? = null,
+    apiVersion: Int = 20240529
 ) {
+    private val DOMAIN = "osu.ppy.sh"
+    private val AUTH_URL = "https://$DOMAIN/oauth"
+    private val stateUuid = UUID.randomUUID()
+    private val logger = LoggerFactory.getLogger(this.javaClass)
 
-    companion object {
-        val client = HttpClient(CIO)
-        const val DOMAIN = "osu.ppy.sh"
-
-        val json = Json {
-            ignoreUnknownKeys = true
-            prettyPrint = true
-            encodeDefaults = true
-            namingStrategy = JsonNamingStrategy.SnakeCase
-        }
+    private val json = Json {
+        ignoreUnknownKeys = true
+        prettyPrint = true
+        encodeDefaults = true
+        namingStrategy = JsonNamingStrategy.SnakeCase
     }
 
-    private suspend fun authorizeApplication(): String {
-        val stateUuid = UUID.randomUUID().toString()
-        val response = client.get("https://$DOMAIN/oauth/authorize") {
+    private suspend fun getCredentialsGrant(): Credentials? {
+        if (clientId == null || clientSecret == null) return null
+        val request = HttpClient(CIO)
+
+        val credentials = createRequestBody().let {
+            val response = request.post("${AUTH_URL}/token") {
+                contentType(ContentType.Application.Json)
+                setBody(json.encodeToString(it))
+            }
+            json.decodeFromString<Credentials>(response.bodyAsText()).also { credentials ->
+                this.accessToken = credentials.accessToken
+                this.refreshToken = credentials.refreshToken
+            }
+        }
+
+        return credentials
+    }
+
+    private suspend fun authorize() {
+        client.get("${AUTH_URL}/authorize") {
             parameter("client_id", clientId)
             parameter("response_type", "code")
             parameter("redirect_uri", redirectUri)
             parameter("state", stateUuid)
             parameter("scope", scope.joinToString(separator = " ") { it.value })
+        }.also {
+            val host = it.request.url.protocolWithAuthority
+            val pathAndQuery = it.request.url.encodedPathAndQuery
+            val uri = URI.create("$host$pathAndQuery")
+
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                Desktop.getDesktop().browse(uri)
+            } else {
+                logger.info("Please open this link in your browser: $uri")
+            }
         }
-        val host = response.request.url.protocolWithAuthority
-        val pathAndQuery = response.request.url.encodedPathAndQuery
-
-        Desktop.getDesktop().browse(URI.create("$host$pathAndQuery"))
-
-        val uri = URI(redirectUri)
-        val server = ServerSocket(uri.port, 0, InetAddress.getByName(uri.host))
-        val accept = server.accept()
-
-        val input = accept.inputStream.bufferedReader().readLine()
-        val code = input.substringAfter("code=").substringBefore("&state=")
-        val state = input.substringAfter("state=").substringBefore(" HTTP")
-
-        if (state != stateUuid) {
-            error("CSRF detected: state mismatch!")
-        }
-
-        accept.outputStream.write("We have received your authorization, you can close this window now.".toByteArray())
-
-        accept.close()
-        server.close()
-
-        return code
     }
 
-    suspend fun create(): OsuKDK {
-        if (grantType == GrantType.AUTHORIZATION_CODE) {
-            this.code = authorizeApplication()
+    private suspend fun getAuthorizationCode(): String {
+        try {
+            return authorize().let {
+                val params = URI(redirectUri).let {
+                    ServerSocket(it.port, 0, InetAddress.getByName(it.host))
+                        .accept().use { request ->
+                            val reader = request.getInputStream().bufferedReader().readLine().split(" ")[1]
+                            val redirectParams = URI("http://${it.host}:${it.port}${reader}")
+                            val params = redirectParams.query.split("&").associate { params ->
+                                val (key, value) = params.split("=")
+                                key to value
+                            }
+                            request.getOutputStream().write("We have received your authorization, you can close this window now.".toByteArray())
+
+                            params
+                        }
+                }
+                if (UUID.fromString(params.get("state")) != stateUuid) {
+                    error("CSRF detected: state mismatch!")
+                }
+                params.get("code") as String
+            }
+        } catch (e: Exception) {
+            throw Exception(e.message)
         }
-        val response = client.post("https://$DOMAIN/oauth/token") {
-            contentType(ContentType.Application.Json)
-            setBody(json.encodeToString(serializer(), this@Authorization))
+    }
+
+    private suspend fun createRequestBody(): Map<String, JsonElement> {
+        var body = mapOf(
+            "client_id" to JsonPrimitive(clientId),
+            "client_secret" to JsonPrimitive(clientSecret),
+            "scope" to JsonPrimitive(ScopesEnum.PUBLIC.value),
+            "grant_type" to JsonPrimitive(grantType.value)
+        )
+        if (grantType == GrantType.AUTHORIZATION_CODE || !redirectUri.isNullOrEmpty()) {
+            body = body.plus(mapOf(
+                "code" to JsonPrimitive(getAuthorizationCode()),
+                "redirect_uri" to JsonPrimitive(redirectUri),
+                "grant_type" to JsonPrimitive(GrantType.AUTHORIZATION_CODE.value)
+            ))
         }
-        val credentials = json.decodeFromString<Credentials>(response.bodyAsText())
-        return OsuKDK(credentials)
+
+        return body
+    }
+
+    private val client = HttpClient(CIO) {
+        defaultRequest {
+            header("x-api-version", apiVersion)
+        }
+
+        install(Logging) {
+            level = LogLevel.NONE
+            logger = Logger.DEFAULT
+        }
+
+        install(Auth) {
+            bearer {
+                if (!accessToken.isNullOrEmpty()) {
+                    loadTokens {
+                        BearerTokens(accessToken as String, refreshToken)
+                    }
+                }
+
+                refreshTokens {
+                    getCredentialsGrant().let {
+                        BearerTokens(
+                            if (it?.accessToken.isNullOrEmpty()) accessToken as String else it.accessToken,
+                            if (it?.refreshToken.isNullOrEmpty()) refreshToken else it.refreshToken
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    fun create(): OsuKDK {
+        return OsuKDK(client)
     }
 }
