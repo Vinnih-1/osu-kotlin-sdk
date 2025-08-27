de198ede87fbbbc0fda44c5978007fcbbe9e84aa
Vinícius Albert <vinifantal@hotmail.com>
2025-08-27 16:54:46 -0300

    fix: failing to convert topic to json

--- src/main/kotlin/endpoints/requests/forum/CreateTopicRequestImpl.kt
+++ src/main/kotlin/endpoints/requests/forum/CreateTopicRequestImpl.kt
@@ -7,7 +7,7 @@ import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import kotlinx.serialization.SerialName
+import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.buildJsonObject
 
 class CreateTopicRequestImpl(
@@ -26,22 +26,23 @@ class CreateTopicRequestImpl(
     override fun endpoint(): String = "forums/topics"
 
     override suspend fun request(client: HttpClient): CreateTopicResponse {
-        val body = buildJsonObject {
-            put("body", json.parseToJsonElement(body))
-            put("forum_id", json.parseToJsonElement(forumId.toString()))
-            put("title", json.parseToJsonElement(title))
-            put("with_poll", json.parseToJsonElement(withPoll.toString()))
-            put("forum_topic_poll[hide_results]", json.parseToJsonElement(pollHideResults.toString()))
-            put("forum_topic_poll[length_days]", json.parseToJsonElement(pollLengthDays.toString()))
-            put("forum_topic_poll[max_options]", json.parseToJsonElement(pollMaxOptions.toString()))
-            put("forum_topic_poll[options]", json.parseToJsonElement(pollOptions.toString()))
-            put("forum_topic_poll[title]", json.parseToJsonElement(pollTitle.toString()))
-            put("forum_topic_poll[vote_change]", json.parseToJsonElement(pollVoteChange.toString()))
+        val bodyJson = buildJsonObject {
+            put("body", JsonPrimitive(body))
+            put("forum_id", JsonPrimitive(forumId.toString()))
+            put("title", JsonPrimitive(title))
+            put("with_poll", JsonPrimitive(withPoll.toString()))
+            put("forum_topic_poll[hide_results]", JsonPrimitive(pollHideResults.toString()))
+            put("forum_topic_poll[length_days]", JsonPrimitive(pollLengthDays.toString()))
+            put("forum_topic_poll[max_options]", JsonPrimitive(pollMaxOptions.toString()))
+            put("forum_topic_poll[options]", JsonPrimitive(pollOptions.toString()))
+            put("forum_topic_poll[title]", JsonPrimitive(pollTitle.toString()))
+            put("forum_topic_poll[vote_change]", JsonPrimitive(pollVoteChange.toString()))
         }
         val response = client.post(this.url) {
             contentType(ContentType.Application.Json)
-            setBody(body.toString())
+            setBody(bodyJson.toString())
         }
+
         return json.decodeFromString<CreateTopicResponse>(response.bodyAsText())
     }
 }
