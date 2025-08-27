94c2ef3610ea53d5abfe3b4982ede369c54e6f18
Vinícius Albert <vinifantal@hotmail.com>
2025-08-27 17:17:34 -0300

    refactor: remove unused runBlocking

--- src/test/kotlin/OsuApiProvider.kt
+++ src/test/kotlin/OsuApiProvider.kt
@@ -1,7 +1,5 @@
 import credentials.Authorization
-import enums.GrantType
 import enums.ScopesEnum
-import kotlinx.coroutines.runBlocking
 
 class OsuApiProvider {
     companion object {
@@ -9,11 +7,8 @@ class OsuApiProvider {
             val clientId = System.getenv("CLIENT_ID").toInt()
             val clientSecret = System.getenv("CLIENT_SECRET")
 
-            runBlocking { Authorization(
-                clientId,
-                clientSecret,
-                "http://localhost:3914",
-                scope = listOf(ScopesEnum.PUBLIC, ScopesEnum.FORUM_WRITE)).create() }
+            Authorization(clientId, clientSecret, "http://localhost:3914",
+                scopes = listOf(ScopesEnum.PUBLIC, ScopesEnum.FORUM_WRITE)).create()
         }
     }
 }
