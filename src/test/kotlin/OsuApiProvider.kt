16188cb7846a2f0c61b8000a7aba7b37574845ee
Vinícius Albert <vinifantal@hotmail.com>
2025-08-28 14:49:23 -0300

    style: applying redirectUri and scopes with apply

--- src/test/kotlin/OsuApiProvider.kt
+++ src/test/kotlin/OsuApiProvider.kt
@@ -7,8 +7,11 @@ class OsuApiProvider {
             val clientId = System.getenv("CLIENT_ID").toInt()
             val clientSecret = System.getenv("CLIENT_SECRET")
 
-            Authorization(clientId, clientSecret, "http://localhost:3914",
-                scopes = listOf(ScopesEnum.PUBLIC, ScopesEnum.FORUM_WRITE)).create()
+            Authorization(clientId, clientSecret)
+                .apply {
+                    redirectUri = "http://localhost:3914"
+                    scopes = listOf(ScopesEnum.PUBLIC, ScopesEnum.CHAT_READ, ScopesEnum.CHAT_WRITE_MANAGE, ScopesEnum.CHAT_WRITE)
+                }.create()
         }
     }
 }
